b832839b65d3c15264234d1a912ac1358db857b3
Daria Orlova <dasha.molodkia@gmail.com>
2023-11-29 18:17:47 +0300

    задача про собаку и два свближающихся человека

---
+++ dog/Program.cs
@@ -0,0 +1,31 @@
+﻿int count = 0;
+int distance = 100000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+        count++;
+    }
+    else
+    {
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+        count++;
+    }
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");   
+
+
+
