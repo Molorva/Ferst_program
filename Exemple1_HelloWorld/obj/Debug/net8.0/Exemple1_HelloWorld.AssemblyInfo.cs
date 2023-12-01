f780481d7f22a94f45e8c021ec5c9f9635d78552
Daria Orlova <dasha.molodkia@gmail.com>
2023-12-01 20:01:13 +0300

    исправление ошибок

--- Exemple1_HelloWorld/obj/Debug/net8.0/Exemple1_HelloWorld.AssemblyInfo.cs
+++ Exemple1_HelloWorld/obj/Debug/net8.0/Exemple1_HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exemple1_HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd8ae840dd3bfae807d0d148a5786214e47a5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exemple1_HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exemple1_HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
