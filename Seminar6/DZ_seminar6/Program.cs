3edab1eb300f60d10f7caf8087343200467eb6ee
Daria Orlova <dasha.molodkia@gmail.com>
2023-12-23 18:28:43 +0300

    дз к 6 семинару

---
+++ Seminar6/DZ_seminar6/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+// a b c => “abcdef”
+// d e f 
+
+// char[,] elements = new char[2, 3]
+// {
+//     {'a', 'b', 'c'},
+//     {'d', 'e', 'f'}
+// };
+
+
+// string GetString()
+// {
+//     string result = "";
+//     for (int i = 0; i < elements.GetLength(0); i++)
+//     {
+//        for (int j = 0; j < elements.GetLength(1); j++)
+//        {
+//         result+= elements[i,j];
+//        }
+//     }
+//     return result;
+// }
+
+// System.Console.WriteLine(GetString());
+
+
+
+
+
+
+// Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+// “aBcD1ef!-” => “abcd1ef!-”
+
+// string str = "aBcD1ef!-";
+// str = str.ToLower(); // метод ToLower() делает все симфолы строчными, а метод ToUpper() делает все симфолы заглавными
+// System.Console.WriteLine(str);
+
+
+
+// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+// System.Console.WriteLine("введите строку");
+// string userInput = Console.ReadLine();
+
+// void GetPalindrom()
+// {
+//     int count = 0;
+//     for (int i = 0; i < userInput.Length / 2; i++)
+//     {
+//         if(userInput[i]==userInput[userInput.Length-i-1]) count++;
+//     }
+//     if(count ==userInput.Length / 2) System.Console.WriteLine("Да");
+//     else System.Console.WriteLine("Нет");
+// } 
+// GetPalindrom();
+
+
+
+// Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+// “Hello my world” => “world my Hello”
+
+string str = "Hello my world";
+
+
+string GetResult()
+{
+    string result = "";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i] == " ")
+        {
+           while (count <i)
+           {
+            result = str[count] + result;
+            count++;
+           } 
+        }
+        count = i;
+    }
+    return result;
+}
---
+++ Seminar6/Program.cs
@@ -0,0 +1,87 @@
+﻿// char [] elements = new char[]{'$', '2', 'F'};
+// string str = "$2f";
+// System.Console.WriteLine(str[0]);
+
+// Задайте массив символов ( тип char[]). Создайте строку из
+// символов  этого массива.
+//указание
+// Конструктор строки вида string(char[]) не использовать.
+
+// char[] elements = new char[] { 't', '8', '4' };
+// string GeStringFromArray()
+// {
+//     string str = "";
+
+//     for (int i = 0; i < elements.Length; i++)
+//     {
+//         str += elements[i];
+//     }
+//     return str;
+// }
+
+// // string result = GeStringFromArray();
+// // System.Console.WriteLine(result);
+// System.Console.WriteLine(GeStringFromArray());
+
+
+// на основе симофольной строки (тип string) сформировать массив
+// симфолов (типа char[]). Вывести массив на экран.
+//Указание
+//Метод строки ToCharArray() не использовать.
+//пример
+// "Hello!" => ['H', 'e', 'l', 'l', 'o', '!']
+
+// System.Console.WriteLine("Введите строчку");
+// string userInput = Console.ReadLine();
+
+// char[] elements = new char[userInput.Length];
+
+// void GetArrayFromString()
+// {
+//     for (int i = 0; i < elements.Length; i++)
+//     {
+//         elements[i] = userInput[i];
+//     }
+    
+// }
+
+// void PrintArray()
+// {
+//     foreach (char item in elements) // работает без индекса, просто перебирает кажды элемент подряд
+//     {
+//         System.Console.Write(item + " ");
+//     }
+// }
+
+// GetArrayFromString();
+// PrintArray();
+
+// Считать строку с консоли, состоящую из латинских
+//букв в нижнем регистре. Выяснить, сколько среди 
+// введенных букв гласных.
+//Пример
+// "hello" => 2
+//"world" = > 1
+
+// System.Console.WriteLine("введите строку");
+// string userInput = Console.ReadLine();
+
+// string vowels = "eyuioa";
+
+// int GetCountVowels()
+// {
+//     int count = 0;
+//     for (int i = 0; i < userInput.Length; i++)
+//     {
+//         for (int j = 0; j < vowels.Length; j++)
+//         {
+//             if(userInput[i]==vowels[j])
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// System.Console.WriteLine(GetCountVowels());
